b0e6288d7d24ac5db1cc7e5df6ca04dd464cf6f0
Hxmusic <hxmusic@foxmail.com>
2017-05-17 13:05:29 +0800

    mody seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             activated: true,
-             activated_at: Time.zone.now)
-
-
 User.create!(name:  "nbtcnet",
              email: "hxmusic@foxmail.com",
              password:              "111111",
@@ -21,32 +13,12 @@ User.create!(name:  "nbtcnet",
              activated: true,
              activated_at: Time.zone.now)
 
+User.create!(name:  "xiangzi",
+             email: "1160167250@qq.com",
+             password:              "111111",
+             password_confirmation: "111111",
+             activated: true,
+             activated_at: Time.zone.now)
 
 
 
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-
-end
-
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
-
-# Following relationships
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
