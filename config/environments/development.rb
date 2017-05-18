7d65f1a1c42bbe38c059010bb3290ac821e79291
Hxmusic <hxmusic@foxmail.com>
2017-05-18 11:24:24 +0800

    Add fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
