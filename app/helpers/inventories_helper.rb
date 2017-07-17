d69e8f1ec1f7990cd6c3a3667e7c57ef93849a7f
Hxmusic <hxmusic@foxmail.com>
2017-07-18 07:49:32 +0800

    delete inventory

--- app/helpers/inventories_helper.rb
+++
@@ -1,5 +0,0 @@
-module InventoriesHelper
-  #include AlbumsHelper
-
-  
-end
--- app/models/inventory.rb
+++
@@ -1,8 +0,0 @@
-class Inventory < ApplicationRecord
-  belongs_to :user
-  has_many :stocks, dependent: :destroy
-  validates :user_id, presence: true
-  validates :sku, presence: true
-  validates :pid, presence: true, default: 0
-  
-end
